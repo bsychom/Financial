de4bc32227d7dcbf492863201667ebbf4334ea8b
mhaddaou <mhaddaou@student.1337.ma>
2023-08-08 17:04:33 +0100

    add img to blog

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,14 +6,31 @@ const Blog = () =>{
     return (
         <section id="blog" className=" bg-color-primary-light">
             <div className="container py-20">
-            <div className="text-center m-auto mb-20 md:w-1/2">
-                   
-                        <h4 className="capitalize font-bold text-color-secondary mb-4">inovation and quality improvement</h4>
-                        <h1 className="capitalize title">latest updates, solutions and company news</h1>
-                        <p>haddaouimed</p>
-                    
+                <div className="text-center m-auto mb-20 md:w-1/2">
+                    <h4 className="capitalize font-bold text-color-secondary mb-4">inovation and quality improvement</h4>
+                    <h1 className="capitalize title">latest updates, solutions and company news</h1>    
+                </div>
+           </div>
+           <div className='grid gap-5 md:grid-cols-2 lg:grid-cols-3 px-6 pb-4'>
+            <div>
+                <div className='lg:h-[40vh] rounded-xl scale-100 overflow-hidden '>
+                    <Image src={blog1} alt='img' className='lg:h-full w-full hover:scale-125 transition duration-300 cursor-pointer'/>
 
                 </div>
+            </div>
+            <div>
+                <div className='lg:h-[40vh] rounded-xl scale-100 overflow-hidden '>
+                    <Image src={blog2} alt='img' className='lg:h-full w-full hover:scale-125 transition duration-300 cursor-pointer'/>
+
+                </div>
+            </div>
+            <div>
+                <div className='lg:h-[40vh] rounded-xl scale-100 overflow-hidden '>
+                    <Image src={blog3} alt='img' className='lg:h-full w-full hover:scale-125 transition duration-300 cursor-pointer'/>
+
+                </div>
+            </div>
+
            </div>
 
         </section>
